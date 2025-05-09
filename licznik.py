a4963808b7e42627b0a65af194d8c4b3d624589f
majstered <biesiada.tv.siedliszcze@wp.pl>
2025-05-09 18:37:59 +0200

    Add files via upload

---
+++ licznik.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+COUNTER_FILE = "visits.txt"
+
+def read_count():
+    if not os.path.exists(COUNTER_FILE):
+        with open(COUNTER_FILE, 'w') as f:
+            f.write("0")
+    with open(COUNTER_FILE, 'r') as f:
+        return int(f.read())
+
+def write_count(count):
+    with open(COUNTER_FILE, 'w') as f:
+        f.write(str(count))
+
+@app.route('/visits')
+def visits():
+    count = read_count() + 1
+    write_count(count)
+    return jsonify({"visits": count})
